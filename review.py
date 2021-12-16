e451fee4420fe56c1025a31a31c7fecbfe31e3ea
ULSSY <96038813+ULSSY@users.noreply.github.com>
2021-12-16 15:37:02 +0900

    2021-12-16

---
+++ review.py
@@ -0,0 +1,7 @@
+echo "# dashboard" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ULSSY/dashboard.git
+git push -u origin main
